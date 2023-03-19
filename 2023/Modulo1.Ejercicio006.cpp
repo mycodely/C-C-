8d2dbfe8f9e4cf27e6c612239c9381a3e30b4cd3
mycodely <122876501+mycodely@users.noreply.github.com>
2023-03-19 08:36:04 -0300

    Create
    Create a program that reads the measurements of a wall (width and height). There is a type of paint to be used to paint the wall that yields 0.5 square meters per liter of paint, which costs $1785 per liter. Create a program that prompts the user for the measurements of the wall and displays the cost of painting the wall.

---
+++ 2023/Modulo1.Ejercicio006.cpp
@@ -0,0 +1,26 @@
+/* 6) Realizar un programa que lea las medidas de una pared (base y altura). Se tiene un tipo de
+pintura a utilizar para pintar dicha pared que rinde 0,5 metros cuadrados por litro de pintura cuyo
+precio es $1785.- por litro. Realizar un programa que le solicite al usuario las medidas de la pared y
+que visualice el costo de pintar dicha pared.
+*/
+#include  <stdio.h>
+float base, altura, result;
+
+int main () {
+    printf("Ingrese base de la pared a pintar\n");
+    scanf("%f", &base);
+    fflush(stdin);
+    
+    printf("Ingrese altura de la pared a pintar\n");
+    scanf("%f", &altura);
+    fflush(stdin);
+    
+    // 0,5mts por 1L --- a 1785$   
+    
+    result = (((base*altura) / 0.5)* 1785);
+    
+    printf("El costo de pintar la pared es: $%.2f", result);
+
+    
+    return 0;
+    }
