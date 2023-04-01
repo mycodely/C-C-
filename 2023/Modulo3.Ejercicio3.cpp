6a4ac34be5cb482952a692f9a125ebcd8f09eefc
mycodely <122876501+mycodely@users.noreply.github.com>
2023-03-31 23:05:38 -0300

    Create Modulo3.Ejercicio3.cpp

---
+++ 2023/Modulo3.Ejercicio3.cpp
@@ -0,0 +1,24 @@
+/* 
+3. Leer 100 números enteros e imprimir la suma de ellos.
+ */
+
+#include <stdio.h>
+int num, aux, aux2;
+
+int main (){
+    
+num = 1;
+aux = 0;
+
+// Test with low number
+    while(num<=4{
+    printf("ingrese un nuevo entero\n");
+    scanf("%d", &num);fflush(stdin);
+    aux = num+ aux;
+    num++;
+    }
+    printf("la suma se acumula: %d \n", aux);                                       ;
+              
+    getchar();
+    return 0; 
+    }
