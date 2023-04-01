dc7effa4e45a18d512ca14bfb0812adc1e72d787
mycodely <122876501+mycodely@users.noreply.github.com>
2023-03-31 23:01:55 -0300

    Create Modulo3.Ejercicio1.cpp

---
+++ 2023/Modulo3.Ejercicio1.cpp
@@ -0,0 +1,17 @@
+/*1. Desarrollar un programa que imprima los números naturales del 1 al 100.
+ */
+
+#include <stdio.h>
+int num;
+
+int main (){
+    
+    num = 1;
+    while(num<=100){
+    printf("numeros hasta el 100, actual: %d \n", num );
+          num++;
+          };
+          getchar();
+          return 0;
+          
+    }
